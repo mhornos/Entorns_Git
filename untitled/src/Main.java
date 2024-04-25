80179bb3b7c7dcf7fe86488c9ad3f0a22f3d77c6
Miguel Angel <m.hornos@sapalomera.cat>
2024-04-25 10:59:57 +0200

    Initial commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        
+        final int nAlumnos = 6;
+        final int nUfs = 4;
+
+        int [][] notes = new int[nAlumnos][nUfs];
+
+        System.out.println(llegirTot(nAlumnos,nUfs,notes));
+    }
+
+    static void llegirTot (int nAlumnos, int nUfs, int[][] notes){
+        leerNotasUfs(nAlumnos, nUfs, notes);
+    }
+
+
+    static int[][] leerNotasUfs (int nAlumnos, int nUfs, int[][] notes){
+        for (int i = 0; i < nAlumnos; i++) {
+                leerNotasPorAlumno(nUfs, notes, i);
+        }
+        return notes;
+    }
+
+    static void leerNotasPorAlumno(int nUfs, int [][] notes, int i){
+        for (int j = 0; j < nUfs; j++) {
+            notes[i][j]= scan.nextInt();
+        }
+    }
+}
